987f32bc9adc8bfa34c755e06c30965630fa7697
Graham Boyle <olayile1999@gmail.com>
2025-06-20 12:22:15 +0100

    updated

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,5 @@
 export const images = {
-  logo: '/logo/logo.webp',
+  logo: '/logo/logo.png',
   banner: '/product/banner.png',
   hero: '/product/hero.png',
   bgImg: '/background/bg.jpeg',
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -20,6 +21,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'export',
 };
 
 export default nextConfig;
